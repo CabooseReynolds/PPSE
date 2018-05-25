4ec70f530a1a5e4099fcded32c4dc3428cac4091
Unknown <caboosereynolds@gmail.com>
2018-05-25 17:08:41 -0600

    scripts

---
+++ Scripts/TurretPHTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EPTrigger : MonoBehaviour {
+	public TurretPHTrigger EPatrol;
+
+	void OnTriggerEnter(Collider other)
+		{
+//		print(this.transform);
+//		print("starting chase");
+		EPatrol.StartChasingPlayer(other.transform);
+        }
+
+	void OnTriggerExit()
+	{
+//		print(this.transform);
+//		print("stopping chase");
+		EPatrol.StopChasingPlayer();
+	}
+}
